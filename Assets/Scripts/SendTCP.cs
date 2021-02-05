151153dc0a01b1e62ebbfe252308b5329f360e5d
Henry <193541T@mymail.nyp.edu.sg>
2021-02-05 22:50:18 +0800

    SendTCP.cs

--- Assets/Scripts/SendTCP.cs
+++ Assets/Scripts/SendTCP.cs
@@ -44,6 +44,31 @@ namespace VirtualChat {
             //SendStr("Hello World!"); //??
         }
 
+        private void Update() {
+            if(client.GetStream().DataAvailable) {
+                byte[] bytes = new byte[client.ReceiveBufferSize];
+                client.GetStream().Read(bytes, 0, client.ReceiveBufferSize); //Returns 0 - client.ReceiveBufferSize //Blocks calling thread until at least 1 byte is read
+                textOutput.text = Encoding.UTF8.GetString(bytes);
+            }
+        }
+
+        private void OnDisable() {
+            client.GetStream().Close();
+            client.Close();
+        }
+
         #endregion
+
+        public void OnSendButtonClicked() {
+            SendStr(textInput.text);
+        }
+
+        private void SendStr(string msg) {
+            NetworkStream stream = client.GetStream();
+            if(stream.CanWrite) {
+                byte[] data = Encoding.UTF8.GetBytes(msg);
+                stream.Write(data, 0, data.Length);
+            }
+        }
     }
 }
