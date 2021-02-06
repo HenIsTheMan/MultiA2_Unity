21c136c89b08f2447e73a4309a67005b797a3ce5
Henry <193541T@mymail.nyp.edu.sg>
2021-02-06 19:31:05 +0800

    BAD

--- Assets/Scripts/SendTCP.cs
+++ Assets/Scripts/SendTCP.cs
@@ -2,6 +2,9 @@
 using System.Text;
 using System.Net.Sockets;
 using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Runtime.Serialization;
 
 namespace VirtualChat {
     internal sealed class SendTCP: MonoBehaviour {
@@ -50,10 +53,19 @@ namespace VirtualChat {
                 byte[] bytes = new byte[client.ReceiveBufferSize];
                 _ = stream.Read(bytes, 0, client.ReceiveBufferSize); //Returns 0 - client.ReceiveBufferSize //Blocks calling thread of execution until at least 1 byte is read
 
+                BinaryFormatter formatter = new BinaryFormatter();
+                object myObj = null;
+                try {
+                    myObj = formatter.Deserialize(stream);
+                } catch(SerializationException e) {
+                    Debug.Log("Deserialization Failed: " + e.Message);
+                }
+                string myStr = myObj as string;
+
                 GameObject msgListItemGO = Instantiate(msgListItemPrefab, GameObject.Find("Content").transform);
 
                 Text textComponent = msgListItemGO.transform.Find("Text").GetComponent<Text>();
-                textComponent.text = ClientData.Username + ": " + Encoding.UTF8.GetString(bytes);
+                textComponent.text = ClientData.Username + ": " + myStr;
 
                 textComponent.color = new Color(ClientData.MyColor.r, ClientData.MyColor.g, ClientData.MyColor.b, 1.0f);
             }
@@ -75,18 +87,27 @@ namespace VirtualChat {
         }
 
         public void OnEnterChat() {
-            SendStr("Hello World!");
+            SendObj("Hello World!");
         }
 
         public void OnSendButtonClicked() {
-            SendStr(textInputBox.text);
+            SendObj(textInputBox.text);
             textInputBox.text = string.Empty;
         }
 
-        private void SendStr(string msg) {
+        private void SendObj(object obj) {
             NetworkStream stream = client.GetStream();
             if(stream.CanWrite) {
-                byte[] data = Encoding.UTF8.GetBytes(msg);
+				MemoryStream memoryStream = new MemoryStream();
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                try {
+                    formatter.Serialize(memoryStream, obj);
+                } catch(SerializationException e) {
+                    Debug.Log("Serialization Failed: " + e.Message);
+                }
+
+                byte[] data = memoryStream.ToArray();
                 stream.Write(data, 0, data.Length);
             }
         }
